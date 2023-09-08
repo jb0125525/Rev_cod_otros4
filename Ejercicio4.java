6fa74970946fa32d4d559cb70b74f2dd0c1bfcd5
Jimena Barranco Medina <Jime@192.168.1.12>
2023-09-08 11:05:55 -0600

    cambios

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,39 +1,73 @@
 package com.generation;
+//importar scanner
+import java.util.Scanner;
 
 public class Codigo4 {
 
-    Scanner s = new Scanner();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    String j1 = s.nextLine();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    Scanner s2 = new Scanner();
-    String j2 = s.nextLine();
-    
-    if (j1 == j2)) {
-      System.out.println("Empate");
-    } else {
-      int g = 2;
-      switch(j1) {
-        case "piedra":
-          if (j2 == "tijeras") {
-            g = 1;
-          }
+	//Mover todo dentro del metodo main
+	public static void main(String[] args) {
+		//Agregar system.in
+		Scanner s = new Scanner(System.in);
+	  
+	  //turno del jugador 1, cambio a sysSystem.out.println
 
-        case "papel":
-          if (j2 == "piedra") {
-            g = 1;
-          
-        case "tijera":
-          if (j2.equals("papel")) {
-            g = 1;
-          }
-          break;
-        default:
-      }
-      System.out.println("Gana el jugador " + g);
+	  System.out.println("Turno del jugador 1 (introduzca piedra, papel o tijeras):  ");
+	  String j1 = s.next();
+	  
+	  
+	  //turno del jugador 2, cambio a sysSystem.out.println
+	  System.out.println("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
+	  String j2 = s.next();
+	  
+	//El scanner 2 no se utiliza
+	//  Scanner s2 = new Scanner(System.in);
+	  
+	  
+	  //eliminar un ")"que tenia de mas
+	  if (j1 == j2) {
+	    System.out.println("Empate");
+	  } else {
+		  //solo declarar variable
+		  int g=2;
+	    
+	    //separar el switch para mas claridad
+	    switch(j1) {
+	      case "piedra":
+	    	 //utilizar equals para las comparaciones de string
+	        if (j2.equals("tijeras")) {
+	          g = 1;
+	          System.out.println("Gana el jugador " + g);
+	        }
+	        
+	        //agregar el break del case1
+	        break;
+
+	      case "papel":
+	        if (j2.equals("piedra")) {
+	          g = 1;
+	          System.out.println("Gana el jugador " + g);
+
+	         //agregar una llave faltante
+	        }
+	      //agregar el break del case2
+	        break;
+	      case "tijera":
+	        if (j2.equals("papel")) {
+	          g = 1;
+	          System.out.println("Gana el jugador " + g);
+
+	        }
+	        break;
+	      default:
+	    	  //falta sentencia por default
+	    	
+	    }
+	    //Agregue el syso a cada caso porque al tenerlo aqui siempre mostraba
+	    //g=2 por lo que siempre ganaba el jugador 2
+	    //System.out.println("Gana el jugador " + g);
+		
+	
+	
     }
-  
-  
+	  }
 }
